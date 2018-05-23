687c5a64d9f36ce09f6e2e4d5786a236fbf1ebbf
Kevin <kevinalh@hotmail.com>
2018-05-22 23:49:28 -0500

    Make Messenger bot talk back.

--- bot/views.py
+++ bot/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import requests
+import json
 
 @csrf_exempt
 def webhook_messenger(request: HttpRequest):
@@ -11,26 +12,30 @@ def webhook_messenger(request: HttpRequest):
         # This is the URL for the Messenger API
         facebook_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='
         facebook_url += settings.PAGE_TOKEN
-        post = request.POST
-        if 'message' in post:
-            # Get useful parameters from the sender
-            psid = post['sender']['id']
-            # Construct the parameters for the API
-            param = {}
-            message = {}
-            recipient = {}
-            param['messaging_type'] = 'RESPONSE'
-            message['text'] = 'Hello friend!'
-            param['message'] = message
-            recipient['id'] = psid
-            param['recipient'] = recipient
-            # Use the API and save the response in a variable
-            requests.post(facebook_url, data = param)
+        query = request.body
+        query = json.loads(query)
+        if 'object' in query:
             response.status_code = 200
+            if query['object'] == 'page':
+                [post] = query['entry'][-1]['messaging']
+                # Get useful parameters from the sender
+                psid = post['sender']['id']
+                # Construct the parameters for the API
+                param = {}
+                message = {}
+                recipient = {}
+                param['messaging_type'] = 'RESPONSE'
+                message['text'] = 'Hello friend!'
+                param['message'] = message
+                recipient['id'] = psid
+                param['recipient'] = recipient
+                # Use the API and save the response in a variable
+                facebook_response = requests.post(facebook_url, json = param)
+                response.status_code = facebook_response.status_code
         # The following response is just to acknowledge the server
         # It's required!
         else:
-            response.status_code = 403
+            response.status_code = 200
     elif request.method == 'GET':
         # Verification used by Facebook.
         verify_token = 'musixbottoken'
