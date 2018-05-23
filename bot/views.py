e92cfd17836b8bbad550e70c9d78172f05a74f3f
Kevin <kevinalh@hotmail.com>
2018-05-22 22:14:46 -0500

    Verification

--- bot/views.py
+++ bot/views.py
@@ -4,22 +4,26 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def webhook_messenger(request: HttpRequest):
     response = HttpResponse(status = 404)
-    if request.method == 'POST':      
+    if request.method == 'POST':
+        # Responding to a Messenger request.
         post = request.POST
         response.content = ''
         if 'page' in post.get('object'):
             response.content = 'OK'
             response.status_code = 200
     elif request.method == 'GET':
+        # Verification used by Facebook.
         verify_token = 'musixbottoken'
-        query = request.GET.get('query')
-        if 'mode' in query and 'token' in query:
+        query = request.GET
+        if 'hub.mode' in query and 'hub.verify_token' in query:
             mode = query['hub.mode'];
             token = query['hub.verify_token'];
             challenge = query['hub.challenge'];
             if mode == 'subscribe' and token == verify_token:
+                # The token matched! Send the challenge back to verify.
                 response.content = challenge
                 response.status_code = 200
             else:
+                # Forbidden: The token didn't match.
                 response.status_code = 403
     return response
--- musixbot/urls.py
+++ musixbot/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('post/', include('bot.urls')),
+    path('webhook/', include('bot.urls')),
 ]
