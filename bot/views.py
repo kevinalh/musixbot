1e23dd6bef746a1670745d60102f345ace85aa4e
Kevin <kevinalh@hotmail.com>
2018-05-22 23:21:10 -0500

    More updates

--- bot/views.py
+++ bot/views.py
@@ -12,25 +12,26 @@ def webhook_messenger(request: HttpRequest):
         facebook_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='
         facebook_url += settings.PAGE_TOKEN
         post = request.POST
-        if 'page' in post.get('object'):
-            # Get useful parameters from the sender
-            psid = post['sender']['id']
-            print(str(psid))
-            # Construct the parameters for the API
-            param = {}
-            message = {}
-            recipient = {}
-            param['messaging_type'] = 'RESPONSE'
-            message['text'] = 'Hello friend!'
-            param['message'] = message
-            recipient['id'] = psid
-            param['recipient'] = recipient
-            # Send the JSON response
-            requests.post(facebook_url, param)
-            # The following response is just to acknowledge the server
-            # It's required!
-            response.content = 'OK'
-            response.status_code = 200
+        if 'object' in post:
+            if 'page' in post['object']:
+                # Get useful parameters from the sender
+                psid = post['sender']['id']
+                print(str(psid))
+                # Construct the parameters for the API
+                param = {}
+                message = {}
+                recipient = {}
+                param['messaging_type'] = 'RESPONSE'
+                message['text'] = 'Hello friend!'
+                param['message'] = message
+                recipient['id'] = psid
+                param['recipient'] = recipient
+                # Send the JSON response
+                requests.post(facebook_url, param)
+                # The following response is just to acknowledge the server
+                # It's required!
+                response.content = 'OK'
+        response.status_code = 200
     elif request.method == 'GET':
         # Verification used by Facebook.
         verify_token = 'musixbottoken'
