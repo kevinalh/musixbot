0c68c24191217fe66645c3d19233ec010c718256
Kevin <kevinalh@hotmail.com>
2018-05-22 19:53:22 -0500

    No static

--- musixbot/settings.py
+++ musixbot/settings.py
@@ -35,8 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.messages'
 ]
 
 MIDDLEWARE = [
@@ -113,8 +112,3 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
